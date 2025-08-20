4bb484eb1386948379a01ba59c08ba053d0b4a86
Simon Karman <mail@simonkarman.nl>
2025-08-20 10:39:25 +0200

    Added comments and add json to express

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
-import express from 'express';
+import express, { json } from 'express';
 
 const app = express();
+app.use(json());
 
+// Health Endpoint
 app.get('/health', (_, res) => {
   res.send('OK');
 });
 
-// Start listening and export a method to close the server
+// Start listening
 // eslint-disable-next-line no-process-env
 const server = app.listen(process.env.PORT || 8000);
+
+// Export a method to close the server
 export default async () => {
   server.close();
 };
